1ca2f19c529c992df292c1408f912a2faab8e193
Ajay AV <ajay@dtcinfotech.com>
2025-12-24 13:53:09 +0530

    basic study

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ReactLogo from './assets/react.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
+ const name = "AJ";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+ const getYear = () => new Date().getFullYear();
+
+return (
+  <div>
+    <h1>Hello {name}</h1>
+    <p>Welcome to React</p>
+    <h2> 3+2 = {3+2}</h2>
+    <h2>Current Year is {getYear()}</h2>
+    <div className="card">
+      <h2 className="title">Product</h2>
+<input type="text" />
+    </div>
+      <img src={ReactLogo} alt="logo" />
+  </div>
+  
+);
 }
 
 export default App
